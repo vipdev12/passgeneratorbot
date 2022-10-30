8291ae48e42087f24658d45f86202f8ac40583ce
Dinislam <aikenamuteshova@gmail.com>
2022-10-30 19:02:36 +0600

    added start func

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ from aiogram import Dispatcher, executor, Bot, types
 import config
 bot = Bot(token=config.configuration["bot_token"])
 dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'], commands_prefix='/!')
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await bot.send_message(message.chat.id, config.configuration["start"])
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
