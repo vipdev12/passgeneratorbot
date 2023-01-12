deb7424da424933ab7a49251aeebc617218372d9
Dinislam <aikenamuteshova@gmail.com>
2023-01-12 21:49:09 +0600

    add requirements for program

--- keyboard.py
+++ keyboard.py
@@ -1,4 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+
 start = ReplyKeyboardMarkup(resize_keyboard=True)
 start.add(KeyboardButton('🎲Рандомный пароль'))
 start.add(KeyboardButton('ℹ️ FAQ'))
@@ -7,7 +9,5 @@ password_1_inline = InlineKeyboardMarkup()
 psw_1 = InlineKeyboardButton(text='📝Буквы(анг)', callback_data="letters")
 psw_2 = InlineKeyboardButton(text='🖊Цифры', callback_data="numbers")
 password_1_inline.add(psw_1, psw_2)
-psw_3 = InlineKeyboardButton(text='📊Буквы с цифрами', callback_data="letters_with_num")
+psw_3 = InlineKeyboardButton(text='🖇Смешанный', callback_data="mixed")
 password_1_inline.add(psw_3)
-psw_4 = InlineKeyboardButton(text='🖇Смешанный', callback_data="mixed")
-password_1_inline.add(psw_4)
